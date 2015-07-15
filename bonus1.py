b579c2c40796ab6db61e18a61355a8895f702dc9
Amr <amr.e.dev@gmail.com>
2015-07-15 23:06:07 +0200

    added txt file and init program

---
+++ bonus1.py
@@ -0,0 +1,4 @@
+
+x = 5
+y = x * x
+print "The square of", x, "=", y
---
+++ common_word.py
@@ -0,0 +1,18 @@
+
+############################################ Get the highest message sender ############################################
+# read input text file
+# pick lines starting with 'From '
+# second word in that line is the sender email address
+# create a dict with key: sender & value: count of messages
+# Loop through the dict and find the highest message sender "Largest count number"
+
+# Get file in
+name = raw_input("Enter file:")
+
+# Default input file
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+for lines in handle:
+	words = lines.split()
+	
