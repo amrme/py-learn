920a812f46d599c021491cbc32a7a8078819a191
Amr <amr.e.dev@gmail.com>
2015-08-08 05:38:10 +0300

    freq done

--- date-dist.py
+++ date-dist.py
@@ -9,6 +9,9 @@ name = raw_input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 
+# empty dict to keep track of (hour, count)
+count = {}
+
 # loop through it 
 for line in handle:
 	# find relevant line: starting with 'From '
@@ -18,4 +21,9 @@ for line in handle:
 			# print words[0]
 			time = words[5].partition(':')
 			hour = time[0]
-			print hour
+			# print hour
+			# get the count 
+			count[hour] = 1 + count.get(hour, 0)
+
+
+# loop and print all the 
